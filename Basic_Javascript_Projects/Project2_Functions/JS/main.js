0313fe058195fec83299ca846341b1970defd4a6
BeeIsland <cameronvwa@gmail.com>
2022-04-27 13:10:23 -0700

    Added second JS wielding website

---
+++ Basic_Javascript_Projects/Project2_Functions/JS/main.js
@@ -0,0 +1,10 @@
+function myFirstFunction() {
+        var bttnTxt = "This is the button text!";
+        document.getElementById("buttonText").innerHTML = bttnTxt;
+}
+
+function myFunction() {
+        var sentence = "I am learning";
+        sentence += " a lot from this book!";
+        document.getElementById("concatenate").innerHTML = sentence;
+}
