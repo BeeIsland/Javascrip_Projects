296bd3a659f50da3bb5dc74201aad45722b695b5
BeeIsland <cameronvwa@gmail.com>
2022-04-27 13:13:29 -0700

    Added comments

--- Basic_Javascript_Projects/Project2_Functions/JS/main.js
+++ Basic_Javascript_Projects/Project2_Functions/JS/main.js
@@ -1,8 +1,10 @@
+//This function Changes the text of the button that's calling it.
 function myFirstFunction() {
         var bttnTxt = "This is the button text!";
         document.getElementById("buttonText").innerHTML = bttnTxt;
 }
 
+//This function concatenates the text and then changes the text of the calling HTML element.
 function myFunction() {
         var sentence = "I am learning";
         sentence += " a lot from this book!";
